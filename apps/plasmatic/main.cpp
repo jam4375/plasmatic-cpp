0a9f67eff223f6c7b6054c1d4884d3902c280c40
Jason Monschke <jam4375@gmail.com>
2023-05-01 10:44:01 -0500

    Add plasmatic CLI test for thermal problems

--- apps/plasmatic/main.cpp
+++ apps/plasmatic/main.cpp
@@ -46,63 +46,63 @@ static auto Run(const nlohmann::json &input) -> int {
 // NOLINTNEXTLINE(bugprone-exception-escape)
 auto main(int argc, char **argv) -> int {
     std::unique_ptr<cxxopts::Options> options;
-    // try {
-    options = std::make_unique<cxxopts::Options>(
-        "Plasmatic", "Plasmatic: finite element modeling and simulation (Version: ${${PROJECT_NAME}_GIT_VERSION})");
+    try {
+        options = std::make_unique<cxxopts::Options>(
+            "Plasmatic", "Plasmatic: finite element modeling and simulation (Version: ${${PROJECT_NAME}_GIT_VERSION})");
 
-    // clang-format off
+        // clang-format off
         options->add_options()
             ("h,help", "Print usage message")
             ("v,verbosity", "Logging verbosity level (Debug, Info, Warn, or Error), default is 'Debug'", cxxopts::value<std::string>())
             ("i,input", "JSON input file", cxxopts::value<std::string>())
         ;
-    // clang-format on
+        // clang-format on
 
-    auto result = options->parse(argc, argv);
+        auto result = options->parse(argc, argv);
 
-    if (result.count("help") == 1) {
-        std::cout << options->help() << std::endl;
-        std::exit(0); // NOLINT(concurrency-mt-unsafe)
-    }
-
-    if (result.count("verbosity") == 1) {
-        auto verbosity = result["verbosity"].as<std::string>();
-
-        if (verbosity == "Debug") {
-            plasmatic::Log::SetVerbosityLevel(plasmatic::Log::Level::Debug);
-        } else if (verbosity == "Info") {
-            plasmatic::Log::SetVerbosityLevel(plasmatic::Log::Level::Info);
-        } else if (verbosity == "Warn") {
-            plasmatic::Log::SetVerbosityLevel(plasmatic::Log::Level::Warn);
-        } else if (verbosity == "Error") {
-            plasmatic::Log::SetVerbosityLevel(plasmatic::Log::Level::Error);
-        } else {
-            throw std::runtime_error("Unknown verbosity level");
+        if (result.count("help") == 1) {
+            std::cout << options->help() << std::endl;
+            std::exit(0); // NOLINT(concurrency-mt-unsafe)
         }
+
+        if (result.count("verbosity") == 1) {
+            auto verbosity = result["verbosity"].as<std::string>();
+
+            if (verbosity == "Debug") {
+                plasmatic::Log::SetVerbosityLevel(plasmatic::Log::Level::Debug);
+            } else if (verbosity == "Info") {
+                plasmatic::Log::SetVerbosityLevel(plasmatic::Log::Level::Info);
+            } else if (verbosity == "Warn") {
+                plasmatic::Log::SetVerbosityLevel(plasmatic::Log::Level::Warn);
+            } else if (verbosity == "Error") {
+                plasmatic::Log::SetVerbosityLevel(plasmatic::Log::Level::Error);
+            } else {
+                throw std::runtime_error("Unknown verbosity level");
+            }
+        }
+
+        nlohmann::json input = {};
+        if (result.count("input") == 1) {
+            std::ifstream in(result["input"].as<std::string>());
+            input = nlohmann::json::parse(in);
+            in.close();
+        }
+
+        // Initialize PETSc:
+        PetscErrorCode ierr = PetscInitialize(&argc, &argv, "", "");
+        plasmatic::Check(ierr == 0, "PETSc returned a non-zero error code: {}", ierr);
+
+        // Entry point:
+        return plasmatic::Run(input);
+    } catch (const cxxopts::option_not_exists_exception &e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        std::cout << std::endl;
+        std::cout << options->help() << std::endl;
+        std::exit(1); // NOLINT(concurrency-mt-unsafe)
+    } catch (const std::exception &e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+        std::cout << std::endl;
+        std::cout << options->help() << std::endl;
+        std::exit(1); // NOLINT(concurrency-mt-unsafe)
     }
-
-    nlohmann::json input = {};
-    if (result.count("input") == 1) {
-        std::ifstream in(result["input"].as<std::string>());
-        input = nlohmann::json::parse(in);
-        in.close();
-    }
-
-    // Initialize PETSc:
-    PetscErrorCode ierr = PetscInitialize(&argc, &argv, "", "");
-    plasmatic::Check(ierr == 0, "PETSc returned a non-zero error code: {}", ierr);
-
-    // Entry point:
-    return plasmatic::Run(input);
-    /* } catch (const cxxopts::option_not_exists_exception &e) {
-         std::cerr << "ERROR: " << e.what() << std::endl;
-         std::cout << std::endl;
-         std::cout << options->help() << std::endl;
-         std::exit(1); // NOLINT(concurrency-mt-unsafe)
-     } catch (const std::exception &e) {
-         std::cerr << "ERROR: " << e.what() << std::endl;
-         std::cout << std::endl;
-         std::cout << options->help() << std::endl;
-         std::exit(1); // NOLINT(concurrency-mt-unsafe)
-     }*/
 }
